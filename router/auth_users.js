6d3cb6b1f5834d70bf828a5f7a32a163ac0a4fd1
Sandesh2211020 <sandesh2211020@iimscollege.edu.np>
2023-04-10 20:36:57 +0545

    First Commit with registration and login

---
+++ router/auth_users.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+  if (users.find((user) => user.username === username)) {
+    return true;
+  }
+};
+
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//only registered users can login
+regd_users.post("/", (req, res) => { 
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign(
+        {
+          data: password,
+        },
+        "access",
+        { expiresIn: 60 * 60 }
+      );
+      res.send(accessToken);
+        req.session.accessToken = accessToken;
+    }
+  } else {
+    return res.status(401).json({ message: "Invalid Login. Check username and password" });
+  }
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  return res.status(300).json({ message: "Yet to be implemented" });
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
