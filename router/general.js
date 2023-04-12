1a2ef6b872720c690787713264a560694c812a3b
Sandesh2211020 <sandesh2211020@iimscollege.edu.np>
2023-04-12 21:07:56 +0545

    updated general method to async/await

--- router/general.js
+++ router/general.js
@@ -4,6 +4,26 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getBookByAuthor = (author) => {
+  let bookList = [];
+  for (let book in books) {
+    if (books[book].author === author) {
+      bookList.push(books[book]);
+    }
+  }
+  return bookList;
+};
+
+const getBookByTitle = (title) => {
+  let bookList = [];
+  for (let book in books) {
+    if (books[book].title == title) {
+      bookList.push(books[book]);
+    }
+  }
+  return bookList;
+};
+
 public_users.post("/register", (req, res) => {
   //Write your code here
   const username = req.body.username;
@@ -22,9 +42,9 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/",async function (req, res) {
   //Write your code here
-  const allBooks = books;
+  const allBooks = await books;
   return res.status(200).json(allBooks);
 });
 
@@ -51,31 +71,25 @@ public_users.get("/isbn/:isbn", function (req, res) {
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   //Write your code here
   const author = req.params.author;
-  let bookList = [];
-  for (let book in books) {
-    if (books[book].author == author) {
-      bookList.push(books[book]);
-    }
-  }
+
+  bookList = await getBookByAuthor(author);
+
   if (bookList.length > 0) {
     return res.status(201).json(bookList);
   }
   return res.status(404).json({ message: "Book not found" });
+
+    
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title",async function (req, res) {
   //Write your code here
   const title = req.params.title;
-  let bookList = [];
-  for (let book in books) {
-    if (books[book].title == title) {
-      bookList.push(books[book]);
-    }
-  }
+  const bookList = await getBookByTitle(title);
   if (bookList.length > 0) {
     return res.status(201).json(bookList);
   }
@@ -83,12 +97,12 @@ public_users.get("/title/:title", function (req, res) {
 });
 
 //  Get book review
-public_users.get("/review/:isbn", function (req, res) {
+public_users.get("/review/:isbn",async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
-    return res.status(201).json(book.reviews);
+  const bookReviews = await books[isbn].reviews;
+  if (bookReviews) {
+    return res.status(201).json(bookReviews);
   }
   return res.status(404).json({ message: "Book not found" });
 });
